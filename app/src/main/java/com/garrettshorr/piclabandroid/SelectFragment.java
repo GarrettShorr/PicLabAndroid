52939c066f4d7103dcda63e85c3e36e8ccba68ff
Garrett <g@garrettshorr.com>
2016-03-26 18:41:23 -0700

    Select an Image from gallery or camera fragment puts a preview in the imagebutton and filterize button can tell if an image has been selected.

--- app/src/main/java/com/garrettshorr/piclabandroid/SelectFragment.java
+++ app/src/main/java/com/garrettshorr/piclabandroid/SelectFragment.java
@@ -1,6 +1,10 @@
 package com.garrettshorr.piclabandroid;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
@@ -12,7 +16,7 @@ import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.Switch;
-import android.widget.ToggleButton;
+import android.widget.Toast;
 
 
 /**
@@ -22,6 +26,8 @@ public class SelectFragment extends Fragment {
 
     public static final int REQUEST_IMAGE_CAPTURE = 0;
     public static final int REQUEST_IMAGE_GALLERY = 1;
+
+    private ImageButton selectImage;
 
     @Nullable
     @Override
@@ -35,7 +41,7 @@ public class SelectFragment extends Fragment {
         camGalSelect.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if(b)
+                if (b)
                     camGalSelect.setText(getResources().getText(R.string.select_switch_on_text));
                 else
                     camGalSelect.setText(getResources().getText(R.string.select_switch_off_text));
@@ -43,7 +49,7 @@ public class SelectFragment extends Fragment {
         });
 
         // ---------- Select Image / Take Picture Button -------- //
-        final ImageButton selectImage =
+        selectImage =
                 (ImageButton) rootView.findViewById(R.id.select_imgbutton_select_picture);
         selectImage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,8 +60,7 @@ public class SelectFragment extends Fragment {
                     if (takePicture.resolveActivity(getActivity().getPackageManager()) != null) {
                         startActivityForResult(takePicture, REQUEST_IMAGE_CAPTURE);
                     }
-                }
-                else {
+                } else {
                     //launch gallery intent
                     Intent choosePicture = new Intent(Intent.ACTION_PICK,
                             MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
@@ -64,8 +69,58 @@ public class SelectFragment extends Fragment {
             }
         });
 
-
+        // -------------------- Filterize Button -------------------- //
+        Button filterizeButton = (Button) rootView.findViewById(R.id.select_button_filterize);
+        filterizeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //check if default image has been changed to indicate selection has happened
+                if (selectImage.getTag().equals("default"))
+                    Toast.makeText(getActivity(), "Change Yo Image!", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(getActivity(), "Image was changed, yo!", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         return rootView;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        switch (requestCode) {
+            case REQUEST_IMAGE_CAPTURE:
+                setImageFromCamera(resultCode, data);
+                break;
+
+            case REQUEST_IMAGE_GALLERY:
+                setImageFromGallery(resultCode, data);
+                break;
+        }
+    }
+
+
+    private void setImageFromGallery(int resultCode, Intent data) {
+        if (resultCode == getActivity().RESULT_OK) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+            Cursor cursor = getActivity().getContentResolver()
+                    .query(selectedImage, filePathColumn, null, null, null);
+            cursor.moveToFirst();
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String imagePath = cursor.getString(columnIndex);
+            cursor.close();
+            selectImage.setImageBitmap(BitmapFactory.decodeFile(imagePath));
+            selectImage.setTag("fromGallery");
+        }
+    }
+
+
+    private void setImageFromCamera(int resultCode, Intent data) {
+        if (resultCode == getActivity().RESULT_OK) {
+            Bitmap previewImage = (Bitmap) data.getExtras().get("data");
+            selectImage.setImageBitmap(previewImage);
+            selectImage.setTag("fromCamera");
+        }
     }
 }
